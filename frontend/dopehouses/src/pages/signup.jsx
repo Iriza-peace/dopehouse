b70e782e191ef5882b6eeeddd4469420b88e7ee5
Iriza-peace <irizapeace.com>
2023-03-01 09:07:32 +0200

    sign up page

--- frontend/dopehouses/src/pages/signup.jsx
+++ frontend/dopehouses/src/pages/signup.jsx
@@ -1,10 +1,144 @@
-import React from 'react'
+import * as React from 'react';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
+import Link from '@mui/material/Link';
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 
-export default function 
-() {
+function Copyright (props){
   return (
-    <div>
-        <h1>Here is my sign up page</h1>
-    </div>
+    <Typography variant="body2" color="text.secondary" align="center">
+      {'Copyright ©'}
+
+      <Link color="inherit" href="https://mui.com/">
+        dopehouse
+        </Link> {' '}
+        {new Date().getFullYear}
+        {'.'}
+    </Typography>
+  )
+}
+
+const theme = createTheme();
+
+export default function Signup() {
+
+  const handleSubmit = (event) =>{
+    event.preventDefault();
+    const data = new FormData(event.currentTarget);
+
+    console.log({
+      fullName: data.get('fullName'),
+      email:data.get('email'),
+      password:data.get('password'),
+      resetpassword:data.get('resetpassword'),
+
+    });
+  }
+  return (
+
+    <ThemeProvider theme = {theme}>
+      <Container component="main" maxWidth="xs">
+        <CssBaseline/>
+        <Box
+        sx={{
+          marginTop:8,
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+        }}
+        
+        >
+          <Avatar sx= {{ m:1, bgcolor: 'primary.main'}}>
+            <LockOutlinedIcon />
+          </Avatar>
+          <Typography component="h1" variant='h5'>
+           SIGN UP
+            </Typography>
+            <Box component="form" onSubmit={handleSubmit} noValidate sx={{mt:1}}>
+              <TextField
+              margin="normal"
+              required
+              fullWidth
+              id="fullName"
+              label="Full Name"
+              name="fullName"
+              autoComplete='fullName'
+              autoFocus
+              />
+
+             <TextField
+              margin="normal"
+              required
+              fullWidth
+              id="email"
+              label="Email Address"
+              name="email"
+              autoComplete='email'
+              autoFocus
+              />
+               <TextField
+              margin="normal"
+              required
+              fullWidth
+              name="password"
+              label="Password"
+              type="password"
+              id="password"
+              autoComplete="current-password"
+            />
+
+<TextField
+              margin="normal"
+              required
+              fullWidth
+              name="resetpassword"
+              label="Reset Password"
+              type="password"
+              id="resetpassword"
+              autoComplete="current-password"
+            />
+
+              <FormControlLabel
+              control={<Checkbox value="remember" color="primary" />}
+              label="Remember me"
+            />
+
+            <Button
+
+            type="submit"
+            fullWidth
+            variant="contained"
+            sx={{mt: 3 , mb:2}}
+            >
+            SIGN UP
+
+            </Button>
+            <Grid container>
+              <Grid item xs>
+              <Link href="#" variant="body2">
+                  Forgot password?
+                </Link>
+              </Grid>
+              <Grid item>
+                <Link href="#" variant="body2">
+                  {"Already have an account? Login"}
+                </Link>
+              </Grid>
+            </Grid>
+            </Box>
+        </Box>
+        <Copyright sx={{mt:8 , mb: 4}} />
+      </Container>
+    </ThemeProvider>
+    
   )
 }
